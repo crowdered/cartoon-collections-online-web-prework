a2c62b54dfd2d389d1e58ba8d680934f5850ee0d
Mark Crowder <crowdered@gmail.com>
2019-03-05 02:23:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,9 +9,9 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(planeteer_calls)
-  
-#end
+  planeteer_calls.any? { |call| call.length > 4 }
+end
 
-#def find_the_cheese# code an argument here
+def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
