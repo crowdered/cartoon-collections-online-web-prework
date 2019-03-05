14b1591a4d64be51ccf303c392301f6ea1468a28
Mark Crowder <crowdered@gmail.com>
2019-03-05 02:09:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,6 +2,7 @@ def roll_call_dwarves(dwarves)
   dwarves.each.with.index(1) do |dwarf, index|
     puts "#{index} #{dwarf}"
 end
+end
 
 #def summon_captain_planet# code an argument here
   # Your code here
